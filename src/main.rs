abef12f89f1c2df98b5b94c125b1b8c31e235d9e
Srinivasan Raghavan <srinivasan.raghavan88@outlook.com>
2020-08-05 10:04:22 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    another_function();
+    another_function1(30);
+    println!("The value of x is: {}", another_function_return_val(30));
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn another_function1(x: i32) {
+    println!("The value of x is: {}", x);
+}
+
+// Return data
+fn another_function_return_val(x: i32) -> i32 {
+    x + 32
+}
+
+fn ownership() {
+    
 }
